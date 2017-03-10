41dc0d865f841bf31c3dfea2704ff378ee08b212
1047239335@qq.com <912513404whh>
2017-03-10 18:19:50 +0800

    主界面文件夹

--- app/src/main/java/com/w77996/hireader/main/MainActivity.java
+++ app/src/main/java/com/w77996/hireader/main/MainActivity.java
@@ -1,7 +1,9 @@
-package com.w77996.hireader;
+package com.w77996.hireader.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.w77996.hireader.R;
 
 public class MainActivity extends AppCompatActivity {
 
