af860950abc67cb893847c753875ba483f5061d2
w77996 <1047239335@qq.com>
2017-03-13 20:52:55 +0800

    笑话大全构架

---
+++ app/src/main/java/com/w77996/hireader/joker/JokerFragment.java
@@ -0,0 +1,7 @@
+package com.w77996.hireader.joker;
+
+/**
+ * Created by Administrator on 2017/3/13.
+ */
+public class JokerFragment  {
+}
---
+++ app/src/main/java/com/w77996/hireader/joker/contract/JokerContract.java
@@ -0,0 +1,8 @@
+package com.w77996.hireader.joker.contract;
+
+/**
+ * Created by Administrator on 2017/3/13.
+ */
+public interface JokerContract {
+
+}
